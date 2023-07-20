2ca31f95943774bf6bf98d5993889f2e258c7f60
BernardoAssad <bernardoassad04@gmail.com>
2023-07-20 15:41:35 -0300

    Alteração nos comentários do operacoes.php e criação do arquivo tipos.php

---
+++ operacoes.php
@@ -0,0 +1,30 @@
+<?php
+
+$idade = 19;
+$idadeDaqui10Anos = $idade + 10;
+$idadeHa5Anos = $idade - 5;
+
+$multiplicaco = 5*5;
+$divisao = 10/2;
+$potencia = 2 ** 3;
+$restoDaDivisao = 10 % 3;
+
+
+echo "Idade daqui a 10 anos (19 + 10): " . $idadeDaqui10Anos;
+echo " ";
+echo "Idade há 5 anos (19 - 5): " . $idadeHa5Anos;
+echo " ";
+echo "Produto da multiplicação (5 * 5): " . $multiplicaco;
+echo " ";
+echo "Quociente da divisão (10 / 2): " . $divisao;
+echo " ";
+echo "Produto da potenciação (2 ** 3): " . $potencia;
+echo " ";
+echo "Resto da divisão (10 % 3): " . $restoDaDivisao;
+echo " ";
+
+/*Nesse código, aprendi a mexer um pouco com operações matemáticas com variáveis em PHP, onde criei a variável idade recebendo 19, e a variável 
+idadeDaqui10Anos que recebe a idade da variável + 10, bem básico, porém útil. Fiz outros exemplos com as demais operações matemáticas.
+
+OBS: Até o momento deste código escrito, não obtive conhecimentos em concatenção e organização de string em PHP, estou improvisando para o código ficar 
+mais legível acreditando que existe melhores formas de mostrar o output. */
