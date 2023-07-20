73e41d699676f8a195de3ab2c5fba69e5bc33108
BernardoAssad <bernardoassad04@gmail.com>
2023-07-20 16:08:25 -0300

    Adicionando arquivo strings.php

---
+++ strings.php
@@ -0,0 +1,16 @@
+<?php
+
+$idade = 19;
+
+echo 'Olá mundo!' . PHP_EOL;
+
+echo "\n";
+
+echo "\t Eu tenho $idade anos.";
+
+
+/* Neste arquivo, obtive conhecimentos interessantes sobre saída de output e strings. O uso da aspas duplas ("") é utilizado para entender caracteres 
+especiais, como por exemplo, \n que serve para pular linha, ou \t que dá um tab na linha. Além dessa funcionalidade, o uso das aspas duplas entende que 
+tudo que está dentro dela, é utilizável, como por exemplo, dá para concatenar string sem precisar usar o ".", Apenas informando que existe a váriavel já
+funciona normalmente. O uso das aspas simples ('') serve para literalmente escrever tudo que está ali dentro no output. Utilizei também uma função da 
+biblioteca do PHP, a PHP_EOL, ela também tem a funcionalidade de pular linha, porém é mais aceito em alguns sistemas operacionais. */
