bb468280e412032b888259631b65073f9a41fa07
BernardoAssad <bernardoassad04@gmail.com>
2023-07-21 12:29:40 -0300

    Alterando comentáros em decisoes.php e criação dos arquvios repeticao.php e repeticao2.php

--- decisoes.php
+++ decisoes.php
@@ -19,5 +19,9 @@ if ($idade >= 18) {
 echo PHP_EOL . "Tchau!";
 
 
-/* Neste arquivo, aprendi sobre condições em PHP, if, else if e else. Sei que existe uma expressão em PHP chamada match, irei me aprofundar nela mais para 
-frente. Além dela, também existe a declaração do switch que serve para controlar fluxos, também irei aprender mais sobre. */
+/* Neste arquivo, aprendi sobre operadores lógicos e condições em PHP, if, else if e else. Sei que existe uma expressão em PHP chamada match, irei me 
+aprofundar nela mais para frente. Além dela, também existe a declaração do switch que serve para controlar fluxos, também irei aprender mais sobre.
+Conceito deste arquivo é bem importante e crucial para o meu desenvolvimento.
+
+OBS: Caso depois do operador lógico e condição tenha apenas uma instrução sendo executada, é possível omitir as chaves, porém não é recomendado.
+*/
