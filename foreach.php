87741ac8a78b71751be9755c7ba8f75f6ff74f67
BernardoAssad <bernardoassad04@gmail.com>
2023-07-21 15:34:37 -0300

    Alterado arquivos foreach.php e listas.php

--- foreach.php
+++ foreach.php
@@ -73,3 +73,70 @@ foreach ($contasCorrentes as $conta) {
 }
 
 // Nesse outro exemplo, quis demonstrar que é possível adicionar um item deste jeito em um array associativo e fazer um foreach nele também.
+
+$contasCorrentes = [
+    12345678900 => [
+        'titular' => 'Barbosa',
+        'saldo' => 5000   
+    ],
+    12345678901 => [
+        'titular' => 'Ademir',
+        'saldo' => 8900
+    ],
+    12345678902 => [
+        'titular' => 'Edmundo',
+        'saldo' => 456
+    ]
+];
+
+$contasCorrentes[] = [
+    'titular' => 'Vavá',
+    'saldo' => 4000
+];
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    echo $cpf . " " . $conta['titular'] . PHP_EOL;
+}
+
+/* Nesse exemplo, quis demonstrar que caso estejamos trabalhando com array associativo, é necessário tomar cuidado, neste caso em específico, não ocorreu
+nenhum problema TÃO GRAVE, nesse aprendizado pois, quando adicionamos um elemento na array sem passar todos os parâmetros necessários, como o CPF, como o
+outro exemplo antes desse, o PHP pega o primeiro índice, neste caso o CPF, e adiciona mais 1 ao final, e como temos CPFs fim 00,01 e 02, o que ele achou foi
+o 03. Mas caso estejamos trabalhando com números de processos, ou até mesmo CPFs reais, isso não seria uma boa ideia. */
+
+$contasCorrentes = [
+    '123.456.789-00' => [
+        'titular' => 'Barbosa',
+        'saldo' => 5000   
+    ],
+    '123.456.789-01' => [
+        'titular' => 'Ademir',
+        'saldo' => 8900
+    ],
+    '123.456.789-02' => [
+        'titular' => 'Edmundo',
+        'saldo' => 456
+    ]
+];
+
+$contasCorrentes[] = [
+    'titular' => 'Vavá',
+    'saldo' => 4000
+];
+
+$contasCorrentes[] = [
+    'titular' => 'Alecssandro',
+    'saldo' => 200
+];
+
+$contasCorrentes['123.456.789.03'] = [
+    'titular' => 'Bebeto',
+    'saldo' => 900
+];
+
+foreach ($contasCorrentes as $cpf => $conta) {
+    echo $cpf . " " . $conta['titular'] . PHP_EOL;
+}
+
+/* Nesse exemplo deu um problema na hora de imprimir o CPF do titular Vavá, pelo motivo de ser uma string, não tem como o PHP fazer um jeito de integrar o
+índice na array de uma forma específica. A forma que ele implementou foi que como não temos nenhum índice numérico neste caso, ele coloca o primeiro índice
+de um array, já que não passamos para ele um parâmetro específico. Então por isso que, ficou 0 Vavá no output, 1 Alecssandro e 123.456.789-03 Bebeto */
