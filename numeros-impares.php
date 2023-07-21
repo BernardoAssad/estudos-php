2c9041121d207b85ee91c90db893c135aa688f9b
BernardoAssad <bernardoassad04@gmail.com>
2023-07-21 12:48:16 -0300

    Criação do arquivo numeros-impares.php, tabuada.php e imc.php

---
+++ numeros-impares.php
@@ -0,0 +1,15 @@
+<?php
+
+for ($contador = 1; $contador < 100; $contador++) {
+    if ($contador % 2 != 0) {
+        echo $contador . PHP_EOL;
+    }
+}
+
+
+/* Desafio de printar todos os números ímpares de 1 a 100. Desafio básico no começo do aprendizado de qualquer linguagem. Utilizei um for de 1 até 100, e um
+operador lógico if para fazer uma divisão do número atual do laço com 2 e caso o resto da divisão fosse diferente de 0, ele seria printado, pois todo número
+ímpar quando dividido por 2 não dá 0. 
+
+Solucionado!
+*/
