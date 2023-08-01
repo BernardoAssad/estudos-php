fc796415d70efc36831ae76d43fbcb75a1b8b6a2
BernardoAssad <bernardoassad04@gmail.com>
2023-08-01 12:51:10 -0300

    Criando arquivo Conta.php

---
+++ poo/Conta.php
@@ -0,0 +1,13 @@
+<?php
+
+class Conta
+{
+    public $cpfTitular;
+    public $nomeTitular;
+    public $saldo;
+}
+
+
+/* Aqui criamos um arquivo chamado Conta.php, repare que o nome deste arquivo começa com letra maiuscula, isso porque, em POO, costumamos colocar a primeira
+letra do nome do arquivo em maiúsculo. 
+*/
